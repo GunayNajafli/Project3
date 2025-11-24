17bae4c6526ec91ef0e14f5aacce26670a88c1a0
Gunay Najafli <gunaynajafli07@gmail.com>
2025-11-24 14:16:26 +0400

    first commit - committing all files to the repository

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Website Loaded Successfully!");
+const modal = document.getElementById('contactModal');
+const btn = document.getElementById('contactBtn');
+const closeBtn = document.getElementById('closeModal');
+
+btn.onclick = () => modal.style.display = "flex";
+closeBtn.onclick = () => modal.style.display = "none";
+
+window.onclick = (e) => {
+  if (e.target == modal) modal.style.display = "none";
+};
+
